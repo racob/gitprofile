f3f202aec62ba82b9f62d117194ff49772f85d25
Eibiel Sardjanto <eibiel260898@gmail.com>
2023-09-02 16:28:21 +0700

    Removed base from vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
 
   // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
 
-  base: '/',
   plugins: [
     react(),
     VitePWA({
